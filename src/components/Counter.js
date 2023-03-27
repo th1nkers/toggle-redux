b4d14b5376944cea4f3bae19a762055d9059dfae
thinkers121 <suryasingh11112003@gmail.com>
2023-03-28 03:06:37 +0530

    added redux-toolkit

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,24 +1,24 @@
 import classes from './Counter.module.css';
 import {useDispatch, useSelector} from 'react-redux';
+import {counterActions} from '../store/counter'
 
 const Counter = () => {
   const dispatch = useDispatch();
-  const counter = useSelector(state => state.counter);
-  const show = useSelector(state => state.showCounter);
+  const counter = useSelector(state => state.counter.counter);
+  const show = useSelector(state => state.counter.showCounter);
 
 
   const incrementHandler = () => {
-    dispatch({type: 'ADD'})
+    dispatch(counterActions.increment())
   }
   const decrementHandler = () => {
-    dispatch({type: 'MINUS'})
+    dispatch(counterActions.decrement())
   }
   const increase = () => {
-    dispatch({type: 'increase', amount: 5})
+    dispatch(counterActions.increase(5)) // {type: SOME_UNIQUE_IDENTIFIER, payload: 10}
   }
-
   const toggleCounterHandler = () => {
-    dispatch({type: 'toggle'});
+    dispatch(counterActions.toggle());
   };
 
   
--- src/store/index.js
+++ src/store/index.js
@@ -1,24 +1,10 @@
-import {createStore} from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
+import counterReducer from './counter'
+import authReducer from './auth'
 
-const initialState = {counter: 0, showCounter: true};
 
-
-const counterReducer = (state=initialState, action) => {
- if(action.type === 'ADD'){
-    return {counter: state.counter + 1, showCounter: state.showCounter} 
- }
- if(action.type === 'MINUS'){
-    return {counter: state.counter - 1,showCounter: state.showCounter}
- }
- if(action.type === 'increase'){
-   return {counter: state.counter + action.amount,showCounter: state.showCounter}
- }
- if(action.type === 'toggle'){
-   return {showCounter: !state.showCounter, counter: state.counter}
- }
- return state;
-}
-
-const store = createStore(counterReducer);
+const store = configureStore({
+  reducer: {counter: counterReducer, auth: authReducer}
+});
 
 export default store;
